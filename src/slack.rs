bf1969ffc2c1ddafd2372ccebd96e316603e3617
Suyashtnt <suyashtnt@gmail.com>
2024-06-19 13:13:42 +0200

    feat: add methods to slack api

---
+++ src/slack.rs
@@ -0,0 +1,52 @@
+use std::io;
+
+use chrono::{DateTime, Utc};
+use errors::SlackClientError;
+use slack_morphism::prelude::*;
+
+pub struct Slack {
+    client: SlackClient<SlackClientHyperConnector<SlackHyperHttpsConnector>>,
+    token: SlackApiToken,
+}
+
+impl Slack {
+    fn new(
+        token: impl Into<SlackApiTokenValue>,
+        team_id: impl Into<SlackTeamId>,
+    ) -> io::Result<Self> {
+        let client = SlackClient::new(
+            SlackClientHyperConnector::new()?.with_rate_control(SlackApiRateControlConfig::new()),
+        );
+        let token: SlackApiToken = SlackApiToken::new(token.into()).with_team_id(team_id.into());
+
+        Ok(Self { client, token })
+    }
+
+    pub async fn update_user_status(
+        &self,
+        username: String,
+        status_text: Option<impl Into<String>>,
+        status_emoji: Option<impl Into<SlackEmoji>>,
+        status_duration: Option<DateTime<Utc>>,
+    ) -> Result<SlackUserProfile, SlackClientError> {
+        let session = self.client.open_session(&self.token);
+
+        let user_request =
+            SlackApiUsersProfileGetRequest::new().with_user(SlackUserId::new(username));
+
+        let user = session.users_profile_get(&user_request).await?;
+
+        let user_update_request = SlackApiUsersProfileSetRequest::new(
+            user.profile
+                .opt_status_emoji(status_emoji.map(Into::into))
+                .opt_status_text(status_text.map(Into::into))
+                .opt_status_expiration(
+                    status_duration.map(|duration| SlackDateTime::new(duration)),
+                ),
+        );
+
+        let updated = session.users_profile_set(&user_update_request).await?;
+
+        Ok(updated.profile)
+    }
+}
